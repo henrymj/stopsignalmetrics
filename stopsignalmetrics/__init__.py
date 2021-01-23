7ed1c026e10eb007da38fd90d08fbe047fa11eac
henrymj <henrymj@stanford.edu>
2021-01-22 16:00:05 -0800

    remove reference to simulate module from init

--- stopsignalmetrics/__init__.py
+++ stopsignalmetrics/__init__.py
@@ -2,4 +2,3 @@ from .stopdata import StopData
 from .ssrtmodel import SSRTmodel
 from .sequence import Sequence, PostStopSlow, Violations
 from .stopsummary import StopSummary
-from .simulatedata import SimulateData
